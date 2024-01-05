596aa517fd86730c93151533227e6f05097136ea
Benjamin Yankey <benkeyben66@gmail.com>
2024-01-05 17:19:02 +0000

    Add request function to get json data from AliExpress API

---
+++ request_json_data.py
@@ -0,0 +1,19 @@
+import requests
+import logging
+
+def request_json_data(params):
+    """
+    This function retrieves json data from the specified URL. 
+    If an error occurs while retrieving the data, the function 
+    logs the error message
+    """
+    url = params['urls'][0]
+    headers = params['headers']
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        return data
+    except Exception as e:
+        logging.error(
+            f"An error occurred while retrieving data from {url}: {e}")
+        return None
