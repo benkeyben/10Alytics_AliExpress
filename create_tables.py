d4b42f040612757522c8852e048c20afef66b118
Benjamin Yankey <benkeyben66@gmail.com>
2024-01-05 17:31:56 +0000

    Add create_tables.py file to create database, schema,tables

---
+++ create_tables.py
@@ -0,0 +1,9 @@
+def create_tables(sql_stmt_list, conn):
+    """
+    This function executes the sql statement object to create tables and schema and closes the
+    connection. It accepts sql statement list, and connection object
+    """
+    cursor = conn.cursor()
+    for stmt in sql_stmt_list[0]:
+        cursor.execute(stmt)
+    conn.commit()
