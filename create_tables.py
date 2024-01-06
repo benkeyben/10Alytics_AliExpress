c9b5a3fac7f78ddda5b5a7779cd1e62ca31f03b1
Benjamin Yankey <benkeyben66@gmail.com>
2024-01-06 01:26:22 +0000

    Update create_tables.py comments

--- create_tables.py
+++ create_tables.py
@@ -2,7 +2,7 @@ def create_tables(sql_stmt_list, conn):
     """
     This function executes the sql statement 
     object to create tables and schema and closes the
-    connection. It accepts sql statement list, and 
+    connection. It accepts sql statements list, and database
     connection object
     """
     cursor = conn.cursor()
